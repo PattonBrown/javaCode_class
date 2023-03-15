41f67b2b02abfd71be4c0e31563536d439eaf3d0
Patton Brown <97097422+PattonBrown@users.noreply.github.com>
2023-03-15 09:33:42 +0800

    Create Fifth_SonOfStringTest.java

---
+++ Fifth_SonOfStringTest.java
@@ -0,0 +1,10 @@
+public class Fifth_SonOfStringTest {
+    public static void testFinal() {
+        System.out.println("SonOfStringTest");
+    }
+
+    public void main(String[] args) {
+        Fifth_StringTreverse test = new Fifth_StringTreverse();
+        test.testFinal();
+    }
+}
