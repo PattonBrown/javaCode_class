d93a6c83b3b6536f8cb5d21c1dc5163dbea2040c
Patton Brown <97097422+PattonBrown@users.noreply.github.com>
2023-04-19 09:20:30 +0800

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,4 +1,3 @@
-
 public class pattern {
     public static boolean validatePhoneNumber(String phoneNumber) {
         String pattern = "^[1][35789]\\d{9}";
@@ -6,7 +5,7 @@ public class pattern {
         return phoneNumber.matches(pattern);
     }
 
-    public static void main(String args) {
+    public static void main(String args[]) {
 
         boolean result = validatePhoneNumber("10086");
         System.out.println(result);
@@ -17,5 +16,13 @@ public class pattern {
 
         title.replaceAll(pattern, "");
         System.out.println(title.replaceAll(pattern, ""));
+
+        String pattern2 = "/\\*.=?\\*/";
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("/*").append("\n").append("*").append("\n").append("*/").append("\n");
+
+        System.out.println(builder.toString());
+        System.out.println("/*my test !*/".matches(pattern2));
     }
 }
