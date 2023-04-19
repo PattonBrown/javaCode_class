d53ff67a1a52c7b2910092482b91877296225dc2
Patton Brown <97097422+PattonBrown@users.noreply.github.com>
2023-04-19 09:34:18 +0800

    Update RegexMatches.java

--- RegexMatches.java
+++ RegexMatches.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,6 +18,19 @@ public class RegexMatches {
             System.out.println("Found value" + m.group(3));
         } else {
             System.out.println("No match");
+        }
+
+
+        String content = "<html><head<title>wtu</title></head>";
+        List<String> srcList = new ArrayList<String>();
+        Pattern p = Pattern.compile("<(img)(.*?)(>|></img>|/", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = p.matcher(content);
+        boolean hasPic = matcher.find();
+        if (hasPic == true) {
+            while (hasPic) {
+                String group = matcher.group(2);
+                System.out.println(group);
+            }
         }
     }
 }
